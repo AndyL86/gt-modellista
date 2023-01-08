6dbf98229e071e32afa4d97fc1a9d693401d0906
AndyL86 <alinehan89@outlook.com>
2023-01-08 12:56:47 +0000

    added bootstrap template to index.html

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Thread
 
+
 class ThreadList(generic.ListView):
     model = Thread
     queryset = Thread.objects.filter(status=1).order_by('-post_date')
