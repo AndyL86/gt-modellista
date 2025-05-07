dabdd26a5610d89ed08ec0258fecee1a78bbdbab
AndyL86 <alinehan89@outlook.com>
2025-05-07 14:50:15 +0100

    revert

--- gtmodellista/settings.py
+++ gtmodellista/settings.py
@@ -92,8 +92,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    "allauth.account.middleware.AccountMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = 'gtmodellista.urls'
